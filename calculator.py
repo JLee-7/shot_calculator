fdcd59bf09b02e3544f5656d483bdb70151ef7e4
JLee-7 <156628326+JLee-7@users.noreply.github.com>
2024-07-18 16:46:32 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,70 @@
+def main():
+    import math
+    magsize = int(input("Rounds in Magazine: "))
+
+    def fourth():
+        print("---------------")
+        print("FOURTH TIMES THE CHARM")
+        x = magsize
+        totalbullets = x
+        while (x >= 4):
+            x = x - 2
+            totalbullets = totalbullets + 2
+        print(totalbullets)
+    fourth()
+
+    def triple():
+        print("---------------")
+        print("TRIPLE TAP")
+        y = magsize
+        totalbullets = y
+        while (y >= 3):
+            y = y - 2
+            totalbullets = totalbullets + 1
+        print(totalbullets)
+    triple()
+
+    def both():
+        print("---------------")
+        print("4th TIMES + TRIPLE TAP")
+        z = magsize
+        totalbullets = (-1**(z+1)) - 12 + (6 * z)
+        print(totalbullets)
+    both()
+
+    def rewind():
+        print("---------------")
+        print("REWIND ROUNDS")
+        a = magsize
+        totalbullets = a
+        bullets = a
+        while (bullets/magsize >= 0.2875):
+            bullets = math.ceil(bullets * 0.6)
+            totalbullets += bullets
+        print(totalbullets)
+    rewind()
+
+main()
+
+#    def rr4th():
+ #       print("rewind + 4th times")
+ #       a = magsize
+ #       totalbullets = a
+#        bullets = a
+ #       while (a >= 4):
+  #          a = a - 2
+   #         totalbullets = totalbullets + 2
+   #         bullets = bullets + 2
+   #     a = math.floor(bullets/2)
+   #     if (a >= 4):
+   #         while (a >= 4):
+    #            a = a - 2
+    #            totalbullets = totalbullets + 2
+    #            bullets = bullets + 2
+    #        a = math.floor(bullets/2)
+    #    else:
+    #        finalshots = a + totalbullets
+    #    print("G",totalbullets + a)
+    #rr4th()
+
+
